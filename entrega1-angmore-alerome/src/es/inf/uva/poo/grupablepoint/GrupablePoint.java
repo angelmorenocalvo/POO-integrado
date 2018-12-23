132cab94d2f9e8898d431cbf4e363562659fa274
angmore <angelmorenocalvo@gmail.com>
2018-12-23 18:09:29 +0100

    PickingPoint completado

--- entrega1-angmore-alerome/src/es/inf/uva/poo/grupablepoint/GrupablePoint.java
+++ entrega1-angmore-alerome/src/es/inf/uva/poo/grupablepoint/GrupablePoint.java
@@ -1,112 +1,59 @@
 package es.inf.uva.poo.grupablepoint;
 
-import java.time.LocalDate;
-
-import es.inf.uva.poo.paquete.Package;
-import es.uva.inf.poo.maps.GPSCoordinate;
 import java.util.ArrayList;
+import es.uva.inf.poo.maps.GPSCoordinate;
+import es.inf.uva.poo.paquete.Package;
 import es.inf.uva.poo.pickingpoint.PickingPoint;
-public class GrupablePoint extends PickingPoint {
 
-	private String identificador;
-	private boolean operativo;
-	private GPSCoordinate localizacionPackageLocker;
-	private  ArrayList<Package> taquillas;
+public class GrupablePoint extends PickingPoint {
+	private  ArrayList<Package> contenedores;
 	private int tamanoMaximo;
 	
-	public GrupablePoint(boolean estado,int tamano, String codigo, GPSCoordinate localizacion) {
+	public GrupablePoint(boolean estado,int tamano, String codigo, GPSCoordinate localizacionp) {
+		super(estado,codigo,localizacionp);
 		if(tamano<0)
 			throw new IllegalArgumentException("El tamano tiene que ser mayor de 0");
-		if(codigo == null)
-			throw new IllegalArgumentException("el codigo debe ser distinto de null");
-		// tanto el identificador como la localizacion se heredan.
-		if(localizacion == null)
-			throw new IllegalArgumentException("Las coordenadas no pueden tomar valor null");
-		operativo = estado;
-		taquillas = new ArrayList<>();
+		
+		contenedores = new ArrayList<>();
 		tamanoMaximo = tamano;
-		localizacionPackageLocker = localizacion;
 	}
 	
 	protected int size() {
-		return taquillas.size();
+		return contenedores.size();
 	}
+	
+	/**
+	 * introducimos un paquete dentro del contenedor.
+	 * @param p un paquete que debe ser distinto de null
+	 * @throws IllegalArgumentException si el paquete es nulo
+	 */
+	public void addPackage(Package p) {
+
+		if(p == null)
+			throw new IllegalArgumentException("El paquete no puede ser null");
+		if(contenedores.size() == tamanoMaximo)
+			throw new IllegalArgumentException("el contenedor esta lleno, no se pueden introducir mas paquetes");
+		contenedores.add(p);
+	}
+	public void addPackage(int i,Package p) {
+		if(i<0)
+			throw new IllegalArgumentException("El tamano tiene que ser mayor de 0");
+		if(i>=getTamano())
+			throw new IllegalArgumentException("El tamano tiene que ser mayor de 0");
+		if(contenedores.size() == tamanoMaximo)
+			throw new IllegalArgumentException("el contenedor esta lleno, no se pueden introducir mas paquetes");
+		contenedores.add(i,p);
+	}
+	
 	
 	protected Package getPackage(int i) {
 		if(i<0)
 			throw new IllegalArgumentException("El tamano tiene que ser mayor de 0");
 		if(i>=getTamano())
 			throw new IllegalArgumentException("El tamano tiene que ser mayor de 0");
-		return taquillas.get(i);
+		return contenedores.get(i);
 	}
-	/**
-	 * modificar el valor de la variable estado
-	 * @param estado para saber si esta abierto o cerrado, es decir entre horario apertura y de cierre
-	 */
-	public void setEstado(boolean estado) {
-		operativo=estado;
-	}
-
-	/**
-	 * consultar el valor del estado
-	 * @return operativo valor del estado 
-	 */
-	public boolean getEstado() {
-		return operativo;
-	}
-
-	/**
-	 * modificar el valor del codigo identificador
-	 * @param codigo
-	 */
-	public void setIdentificador(String codigo) {
-		if(codigo == null)
-			throw new IllegalArgumentException("Codigo debe ser != null");		
-		identificador=codigo;
-	}
-	/**
-	 * Consulta el numero de las taquillas que se encuentran ocupadas en  el package locker
-	 * @return valor entero positivo
-	 */
 	
-	public int getOcupadas() {
-		return  taquillas.size();
-	}
-	/**
-	 * Consulta el numero de las taquillas que se encuentran vacias en  el package locker
-	 * @return valor entero positivo (tendremos que ajustar esto para que sea asi)
-	 */
-	
-	public int getVacias(){
-		return  tamanoMaximo- taquillas.size();
-	}
-	/**
-	 * Consultar el codigo identificador 
-	 * @return identificador
-	 */
-	public String getIdentificador() {
-		return identificador;
-	}
-
-	/**
-	 * modifica el valor de las coordenadas en las que se encuentra un packagelocker
-	 * @param x
-	 * @param y
-	 */
-	public void setCoordenadas(GPSCoordinate localizacion) {
-		if(localizacion == null)
-			throw new IllegalArgumentException("Las coordenadas no pueden tomar valor null");
-		
-		localizacionPackageLocker = localizacion;
-	}
-
-	/**
-	 * Consulta el valor de las coordenadas 
-	 * @return localizacionPackageLocker coordenadas del packagelocker
-	 */
-	public GPSCoordinate getCoordenadas() {
-		return localizacionPackageLocker;
-	}
 	
 	/**
 	 * Busqueda de un paquete a traves de su codigo de identificacion.
@@ -117,61 +64,36 @@ public class GrupablePoint extends PickingPoint {
 	 * 
 	 * @return retorna el paquete con el identificador introducido.
 	 */
-	
+	@Override
 	public Package getPackage(String codigo) {
 		if(codigo == null)
 			throw new IllegalArgumentException("El codigo no puede ser null");
 		boolean encontrado = false;
-		for(Package i : taquillas) {
+		for(Package i : contenedores) {
 			if (codigo == i.getIdentificador())
 				encontrado = true;
 		}
 		if(!encontrado)
 			throw new IllegalArgumentException("Debe existir un paquete con el identificador");
-		for(Package i : taquillas) {
+		for(Package i : contenedores) {
 			if (codigo == i.getIdentificador())
 				return i;
 		}
 		return null;
 	}
-
-	/**
-	 * Consulta el numero de las taquillas de las que consta el package locker
-	 * @return valor entero positivo (tendremos que ajustar esto para que sea asi)
-	 */
-	public int getTamano() {
-		return tamanoMaximo;
-	}
 	
-	public void cambiarTam(int tamano) {
-		if(tamano<0)
-			throw new IllegalArgumentException("El tamano tiene que ser mayor de 0");
-		tamanoMaximo = tamano;
-		
-	}
-
-	/**
-	 * Calcula la distancia entre dos GrupablePoint o hijos suyos.
-	 * @param otro tipo GrupablePoint o hijos suyos.
-	 * 
-	 * @return Un doble con la distancia entre ambos.
-	 */
-	public double distance(GrupablePoint otro) {
-		return (localizacionPackageLocker.getDistanceTo(otro.getCoordenadas()));
-	}
-
-
 	/**
 	 * Sacamos un paquete de una taquilla buscandolo por su codigo
 	 * @param codigo String que debe ser distinto de {@code  null}
 	 * @throws IllegalArgumentException si no existe paquete con ese codigo
 	 */
+	@Override
 	public void eliminaPaquete(String codigo) { 
 		if(codigo == null) {
 			throw new IllegalArgumentException("Debe ser diferente de null");
 		}
 		boolean encontrado = false;
-		for(Package i : taquillas) {
+		for(Package i : contenedores) {
 			if (codigo == i.getIdentificador())
 				encontrado = true;
 		}
@@ -179,101 +101,93 @@ public class GrupablePoint extends PickingPoint {
 			throw new IllegalArgumentException("Debe existir un paquete con el identificador");
 		
 		
-		for(int i = 0; i<taquillas.size();i++) {
-			if (taquillas.get(i).getIdentificador().equals(codigo)) {
-				taquillas.add(i, null);
+		for(int i = 0; i<contenedores.size();i++) {
+			if (contenedores.get(i).getIdentificador().equals(codigo)) {
+				contenedores.add(i, null);
 			}
 		}
 	}
-
+	
 	/**
 	 * 
 	 * @param codigoDeBusqueda
 	 */
+	@Override
 	public void packagePicked(String codigoDeBusqueda) {
 		if(codigoDeBusqueda == null) {
 			throw new IllegalArgumentException("identificador debe ser != null");
 		}
-		for(Package i : taquillas) { // HABRA QUE MIRAR QUE NO ESTE RECOGIDO NI QUE ESTE MANDADO A CENTRAL				
+		for(Package i : contenedores) { // HABRA QUE MIRAR QUE NO ESTE RECOGIDO NI QUE ESTE MANDADO A CENTRAL				
 				if (i.getIdentificador() == codigoDeBusqueda ) {
-					if(taquillas.get(posicionPackage(i)).getRecogido())
+					if(contenedores.get(posicionPackage(i)).getRecogido())
 						throw new AssertionError("El paquete ya ha sido recogido por el cliente");
-					if(taquillas.get(posicionPackage(i)).getDevuelto())
+					if(contenedores.get(posicionPackage(i)).getDevuelto())
 						throw new AssertionError("El paquete se ha devuelto al centro");
 					i .setRecogido(true);
 				}
 		}
 	}
-
-	public void toCentral(String codigoDeBusqueda) {
-		if(codigoDeBusqueda == null) {
-			throw new IllegalArgumentException("llamada incorrecta al identificador del paquete, identificador == null");
+		
+		/**
+		 * Consulta el numero de las taquillas que se encuentran ocupadas en  el package locker
+		 * @return valor entero positivo
+		 */
+		@Override
+		public int getOcupadas() {
+			return  contenedores.size();
 		}
-		for(Package i : taquillas) {
-				if (i.getIdentificador() == codigoDeBusqueda) {
-					if(taquillas.get(posicionPackage(i)).getRecogido())
-						throw new AssertionError("El paquete ya ha sido recogido por el cliente");
-					if(taquillas.get(posicionPackage(i)).getDevuelto())
-						throw new AssertionError("El paquete se ha devuelto al centro");
-					i .setDevuelto(true);
-				}
+		/**
+		 * Consulta el numero de las taquillas que se encuentran vacias en  el package locker
+		 * @return valor entero positivo (tendremos que ajustar esto para que sea asi)
+		 */
+		@Override
+		public int getVacias(){
+			return  tamanoMaximo- contenedores.size();
 		}
-		throw new IllegalArgumentException("El paquete no exite");
 	
-	}
-	/**
-	 * introducimos un paquete dentro del contenedor.
-	 * @param p un paquete que debe ser distinto de null
-	 * @throws IllegalArgumentException si el paquete es nulo
-	 */
-	public void addPackage(Package p) {
-
-		if(p == null)
-			throw new IllegalArgumentException("El paquete no puede ser null");
-		if(taquillas.size() == tamanoMaximo)
-			throw new IllegalArgumentException("el contenedor esta lleno, no se pueden introducir mas paquetes");
-		taquillas.add(p);
-	}
-	
-	public void addPackage(int i,Package p) {
-		if(i<0)
-			throw new IllegalArgumentException("El tamano tiene que ser mayor de 0");
-		if(i>=getTamano())
-			throw new IllegalArgumentException("El tamano tiene que ser mayor de 0");
-		if(taquillas.size() == tamanoMaximo)
-			throw new IllegalArgumentException("el contenedor esta lleno, no se pueden introducir mas paquetes");
-		taquillas.add(i,p);
-	}
-	
-//mira si esto tiene que ir en el paquete
-	/**
-	 * Comprueba si el paquete ha pasado de fecha
-	 * @param hoy
-	 * @param paquete
-	 * @throws IllegalArgumentException si {hoy==null}
-	 */
-	public void expiredPackage(LocalDate hoy, Package paquete) {
-		if (hoy == null)
-			throw new IllegalArgumentException("La fecha de hoy no puede ser null.");
-		if(paquete.expirado(hoy))
-			paquete.setDevuelto(true);
-	}
-
-	/**
-	 * Retorna la posicion en la que se encuentra el paquete
-	 * @param paquetebuscado
-	 * @return la posicion
-	 * @throws AssertionError si no encuentra la posicion del paquete
-	 */
-	protected int posicionPackage(Package paquetebuscado) {
-		if(paquetebuscado == null) {
-			throw new IllegalArgumentException("El paquete introducido es null, introduzca un paquete creado.");
+		public int getTamano() {
+			return tamanoMaximo;
 		}
-		for(int i = 0;i<taquillas.size();i++) {
-			if(paquetebuscado == taquillas.get(i))
-				return i;
+		
+		public void cambiarTam(int tamano) {
+			if(tamano<0)
+				throw new IllegalArgumentException("El tamano tiene que ser mayor de 0");
+			tamanoMaximo = tamano;
+			
 		}
-		throw new AssertionError("no encontrada la posicion del paquete");
+		
+		public void toCentral(String codigoDeBusqueda) {
+			if(codigoDeBusqueda == null) {
+				throw new IllegalArgumentException("llamada incorrecta al identificador del paquete, identificador == null");
+			}
+			for(Package i : contenedores) {
+					if (i.getIdentificador() == codigoDeBusqueda) {
+						if(contenedores.get(posicionPackage(i)).getRecogido())
+							throw new AssertionError("El paquete ya ha sido recogido por el cliente");
+						if(contenedores.get(posicionPackage(i)).getDevuelto())
+							throw new AssertionError("El paquete se ha devuelto al centro");
+						i .setDevuelto(true);
+					}
+			}
+			throw new IllegalArgumentException("El paquete no exite");
+		
+		}
+		
+		/**
+		 * Retorna la posicion en la que se encuentra el paquete
+		 * @param paquetebuscado
+		 * @return la posicion
+		 * @throws AssertionError si no encuentra la posicion del paquete
+		 */
+		protected int posicionPackage(Package paquetebuscado) {
+			if(paquetebuscado == null) {
+				throw new IllegalArgumentException("El paquete introducido es null, introduzca un paquete creado.");
+			}
+			for(int i = 0;i<contenedores.size();i++) {
+				if(paquetebuscado == contenedores.get(i))
+					return i;
+			}
+			throw new AssertionError("no encontrada la posicion del paquete");
+		}
 	}
 
-}
--- entrega1-angmore-alerome/src/es/inf/uva/poo/pickingpoint/PickingPoint.java
+++ entrega1-angmore-alerome/src/es/inf/uva/poo/pickingpoint/PickingPoint.java
@@ -1,5 +1,129 @@
 package es.inf.uva.poo.pickingpoint;
 
-public class PickingPoint {
+
+import java.time.LocalDate;
+
+
+import es.inf.uva.poo.paquete.Package;
+import es.uva.inf.poo.maps.GPSCoordinate;
+public abstract class PickingPoint {
+	private String identificador;
+	private boolean operativo;
+	private GPSCoordinate localizacion;
+	
+	
+	public PickingPoint(boolean estado, String codigo, GPSCoordinate localizacionp) {
+		if(codigo == null)
+			throw new IllegalArgumentException("el codigo debe ser distinto de null");
+		
+		if(localizacionp == null)
+			throw new IllegalArgumentException("Las coordenadas no pueden tomar valor null");
+		operativo = estado;
+		identificador = codigo;
+		localizacion = localizacionp;
+	}
+	
+	
+	
+	//protected abstract  Package getPackage(int i);
+	public abstract Package getPackage(String codigo);
+	public abstract void eliminaPaquete(String codigo);
+	public abstract void packagePicked(String codigoDeBusqueda);
+	public abstract void addPackage(Package p);
+	public abstract int getVacias();
+	public abstract int getOcupadas();
+	public abstract void toCentral(String codigoDeBusqueda);
+	
+	
+
+
+	
+	
+	/**
+	 * modificar el valor de la variable estado
+	 * @param estado para saber si esta abierto o cerrado, es decir entre horario apertura y de cierre
+	 */
+	public void setEstado(boolean estado) {
+		operativo=estado;
+	}
+	
+	/**
+	 * consultar el valor del estado
+	 * @return operativo valor del estado 
+	 */
+	public boolean getEstado() {
+		return operativo;
+	}
+	
+	/**
+	 * modificar el valor del codigo identificador
+	 * @param codigo
+	 */
+	public void setIdentificador(String codigo) {
+		if(codigo == null)
+			throw new IllegalArgumentException("Codigo debe ser != null");		
+		identificador=codigo;
+	}
+	
+	
+	/**
+	 * Consultar el codigo identificador 
+	 * @return identificador
+	 */
+	public String getIdentificador() {
+		return identificador;
+	}
+
+	/**
+	 * modifica el valor de las coordenadas en las que se encuentra un packagelocker
+	 * @param x cambia la localizacion
+	 * 
+	 */
+	public void setCoordenadas(GPSCoordinate localizacionp) {
+		if(localizacion == null)
+			throw new IllegalArgumentException("Las coordenadas no pueden tomar valor null");
+		
+		localizacion = localizacionp;
+	}
+	
+	/**
+	 * Consulta el valor de las coordenadas 
+	 * @return localizacionPackageLocker coordenadas del packagelocker
+	 */
+	public GPSCoordinate getCoordenadas() {
+		return localizacion;
+	}
+	/**
+	 * Consulta el numero de las taquillas de las que consta el package locker
+	 * @return valor entero positivo (tendremos que ajustar esto para que sea asi)
+	 */
+	
+	
+	/**
+	 * Calcula la distancia entre dos GrupablePoint o hijos suyos.
+	 * @param otro tipo GrupablePoint o hijos suyos.
+	 * 
+	 * @return Un doble con la distancia entre ambos.
+	 */
+	public double distance(PickingPoint otro) {
+		return (localizacion.getDistanceTo(otro.getCoordenadas()));
+	}
+	
+	
+
+	//mira si esto tiene que ir en el paquete
+		/**
+		 * Comprueba si el paquete ha pasado de fecha
+		 * @param hoy
+		 * @param paquete
+		 * @throws IllegalArgumentException si {hoy==null}
+		 */
+		public void expiredPackage(LocalDate hoy, Package paquete) {
+			if (hoy == null)
+				throw new IllegalArgumentException("La fecha de hoy no puede ser null.");
+			if(paquete.expirado(hoy))
+				paquete.setDevuelto(true);
+		}
+	
 
 }
